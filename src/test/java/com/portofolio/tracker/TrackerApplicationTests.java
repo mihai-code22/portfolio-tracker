c1c12f767cdd2bec506c6e1eee11c0b8cd565197
Baescu Mihai <baescu.mihai.dev@gmail.com>
2026-02-21 14:20:11 +0200

    project setup - Spring Initializr

---
+++ src/test/java/com/portofolio/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portofolio.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
